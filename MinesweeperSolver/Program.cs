5e9820c34ce0968458a6eaae2978de575c19ad2d
Justin McCarthy <Justin00892@gmail.com>
2019-06-07 14:36:18 -0500

    Initial Upload part 2

--- MinesweeperSolver/Program.cs
+++ MinesweeperSolver/Program.cs
@@ -16,7 +16,7 @@ namespace MinesweeperSolver
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
